fc091471b3d818a80ba116f0ac6e16233889df50
Veronica Alley <veronica.aubry.alley@gmail.com>
2016-03-07 17:40:35 -0800

    add files

---
+++ main.js
@@ -0,0 +1,22 @@
+Ron Wyden: {
+  sectors: {
+    money_from_pacs: 589360,
+    money_from_indivs: 1109475,
+    sector_name: 'Finance/Insur/RealEst'
+  },
+  {
+    money_from_pacs: 782240,
+    money_from_indivs: 479476,
+    sector_name: 'Health'
+  },
+  {
+    money_from_pacs: 184676,
+    money_from_indivs: 638566,
+    sector_name: 'Lawyers & Lobbyists'
+  },
+  {
+    money_from_pacs: 104350,
+    money_from_indivs: 71799,
+    sector_name: 'Transportation'
+  }
+}
