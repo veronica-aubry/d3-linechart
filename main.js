16066496e134ea7ff69c659c9c08455fb5a00c0a
Veronica Alley <veronica.aubry.alley@gmail.com>
2016-03-20 13:54:26 -0700

    add axis to scatterplot

--- main.js
+++ main.js
@@ -2,15 +2,15 @@
 
 
 var data = _.map( [58.936,78.224,18.4676,10.435, 30.552, 40.765, 20.866, 90.34],
-function(d) {
+function(d, index) {
   return {
-    x: d + 3,
-    y: 250 - d,
+    x: index * 10,
+    y: d,
     r: 2
   };
 });
 
-var margin = {top:0, right:0, bottom:15, left:0};
+var margin = {top:10, right:10, bottom:30, left:30};
 
 var h = 250 - margin.top - margin.bottom;
 
@@ -21,17 +21,33 @@ var yScale = d3.scale.linear()
     return data.y;
   }
   )])
-  .range([0, h])
+  .range([h,0])
 
 var xScale = d3.scale.linear()
   .domain([0,100])
   .range([0,w])
 
+var xAxis = d3.svg.axis()
+  .scale(xScale)
+  .orient('bottom')
+  .ticks(10)
+  .tickSize(12)
+  .outerTickSize(2)
+  .tickPadding(0)
+
+var yAxis = d3.svg.axis()
+  .scale(yScale)
+  .orient('left')
+  .ticks(10)
+  .tickSize(3)
+  .outerTickSize(2)
+  .tickPadding(0)
+
 var colorScale = d3.scale.linear()
   .domain([0, d3.max(data)])
   .range(['gold', 'tomato'])
 
-var svg = d3.select('#barChart').append('svg')
+var svg = d3.select('#content').append('svg')
   .attr('width', w + margin.left + margin.right)
   .attr('height', h + margin.top + margin.bottom)
   .append('g')
@@ -58,3 +74,12 @@ svg.selectAll('circle')
 .attr('fill', function(data, index){
   return colorScale(data)
 })
+
+svg.append('g')
+  .attr('class', 'x axis')
+  .attr('transform', 'translate(0 '+(h+0)+')')
+  .call(xAxis)
+
+svg.append('g')
+  .attr('class', 'y axis')
+  .call(yAxis)
